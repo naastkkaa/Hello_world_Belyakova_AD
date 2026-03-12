91b394fc3e6c500c4bccacf448762c091ea899e5
Настя Белякова <nastena.belyakova.07@gmail.com>
2026-03-12 21:40:02 +0300

    Delete task_2_0 directory

--- task_2_0/task_2-0_5.py
+++
@@ -1,4 +0,0 @@
-name = "Настя"
-group = 47312004_50002
-score = 111
-print(f"студент {name} , из группы {group} , получил {score}")
--- task_2_0/Белякова АД/main.py
+++
@@ -1,6 +0,0 @@
-name = "стейси"
-a = 10
-b = 5
-resuld = a + b
-print(F"привет, {name}!")
-print(F"Результат сложения {a} + {b} равен {resuld}")
